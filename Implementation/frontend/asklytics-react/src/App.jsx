866aa08d1f7164fc7045396b201b421638757306
th-aslam <tamoor.h.aslam@ooma.com>
2025-02-09 18:58:31 -0800

    [Frontend]: Fixing language

--- Implementation/frontend/asklytics-react/src/App.jsx
+++ Implementation/frontend/asklytics-react/src/App.jsx
@@ -10,6 +10,7 @@ const App = () => (
     helloMessage={
       'Hello from Asklytics - COMP-4495 - Applied Research Project'
     }
+    placeholder={'Ask me anything about your website analytics'}
     request={async (messages) => {
       const mockedData = `This is a dummy response ${messages.length}`;
       return new Response(mockedData);
