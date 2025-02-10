13326f63662785c0affeb6510f02e3fa596eaec8
th-aslam <tamoor.h.aslam@ooma.com>
2025-02-09 21:51:03 -0800

    [Frontend]: Setup app color theme

--- Implementation/frontend/asklytics-react/src/main.jsx
+++ Implementation/frontend/asklytics-react/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ConfigProvider, theme } from 'antd';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="573818600958-kaj7vbphtvg4883he4r8s79qk4qglurd.apps.googleusercontent.com">
-      <App />
+      <ConfigProvider theme={{ token: { colorPrimary: '#1677ff' } }}>
+        <App />
+      </ConfigProvider>
     </GoogleOAuthProvider>
   </StrictMode>
 )
