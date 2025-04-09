08a8fb3dabaef990a1844275ed239c4187354a7f
th-aslam <tamoor.h.aslam@ooma.com>
2025-04-08 21:28:23 -0700

    refactoring

--- Implementation/Asklytics/app/src/app/layout.tsx
+++ Implementation/Asklytics/app/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className}  antialiased`}
+        className={`${inter.className} antialiased`}
       >
         <GoogleOAuthProvider clientId="573818600958-kaj7vbphtvg4883he4r8s79qk4qglurd.apps.googleusercontent.com">
           {children}
